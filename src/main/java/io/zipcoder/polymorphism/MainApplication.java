2f782033fba8ba6ef24d6e551a029a0c41382751
igordon215 <igordon215@gmail.com>
2024-07-15 08:15:41 -0400

    classes+tests

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,4 +1,7 @@
 package io.zipcoder.polymorphism;
+
+import java.util.Scanner;
+
 //Part 1:
 //Create a program that asks the user how many pets they have.
 //Once you know how many pets they have, ask them what kind of pet each one is,
@@ -6,7 +9,16 @@ package io.zipcoder.polymorphism;
 //and print out the list at the end; we'll modify this in part 3.
 public class MainApplication {
 
-    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How Many Pets Do You Have??");
+        int  numberOfPets = scanner.nextInt();
+        scanner.nextLine();
+
+
+    }
 
 
 
---
+++ src/main/java/io/zipcoder/polymorphism/Pet.java
@@ -0,0 +1,5 @@
+package io.zipcoder.polymorphism;
+//The Pet class must have a speak method that each subclass overrides.
+//The Pet class must have a name field with setters and getters.
+public class Pet {
+}
