1570bca23dd00bcf7c8adb1a07a820702a64876a
igordon215 <igordon215@gmail.com>
2024-07-15 08:05:15 -0400

    inital_commit

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,4 +1,13 @@
 package io.zipcoder.polymorphism;
-
+//Part 1:
+//Create a program that asks the user how many pets they have.
+//Once you know how many pets they have, ask them what kind of pet each one is,
+//along with each pet's name. For now your program should just hold onto the user input
+//and print out the list at the end; we'll modify this in part 3.
 public class MainApplication {
+
+    
+
+
+
 }
