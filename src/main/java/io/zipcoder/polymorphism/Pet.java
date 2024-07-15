32fc3c6cbca3eeba0f2491733fcfd6da85bae0cb
igordon215 <igordon215@gmail.com>
2024-07-15 08:45:36 -0400

    pet_speak

--- src/main/java/io/zipcoder/polymorphism/Pet.java
+++ src/main/java/io/zipcoder/polymorphism/Pet.java
@@ -1,5 +1,28 @@
 package io.zipcoder.polymorphism;
+
+
 //The Pet class must have a speak method that each subclass overrides.
 //The Pet class must have a name field with setters and getters.
+
+
 public class Pet {
+
+    private String name;
+
+    public Pet(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String newName) {
+        this.name = name;
+    }
+
+    public String speak() {
+        return "Feed Me Human!";
+    }
 }
--- src/test/java/io/zipcoder/polymorphism/PetTest.java
+++ src/test/java/io/zipcoder/polymorphism/PetTest.java
@@ -1,7 +1,42 @@
 package io.zipcoder.polymorphism;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PetTest {
+
+    @Test
+    public void getName(){
+
+        String expectedPetName = "Smith";
+        Pet pet = new Pet(expectedPetName);
+
+        String actualName = pet.getName();
+
+        Assert.assertEquals(expectedPetName, actualName);
+    }
+
+    @Test
+    public void setName(){
+
+        Pet pet = new Pet("Ollie");
+        String newName = "Ollie";
+
+        pet.setName(newName);
+
+        Assert.assertEquals(newName, pet.getName());
+    }
+
+    @Test
+    public void speak(){
+
+        Pet pet  = new Pet("Bruce McBarker");
+
+        String talkingPets = pet.speak();
+
+        Assert.assertEquals("Feed Me Human!", talkingPets);
+    }
 
 }
